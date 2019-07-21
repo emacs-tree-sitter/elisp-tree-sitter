d1dad5f00b416536f9e4148feffb59544af8de8e
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-07-21 17:56:06 +0700

    Expose more parser methods

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,9 @@
-use emacs::{defun, Result, Value, IntoLisp, ResultExt};
+use emacs::{defun, Result, Value, IntoLisp, ResultExt, Vector};
 use emacs::failure;
 
 use tree_sitter::{Parser, Point};
 
-use crate::types::{SharedTree, shared, Language};
+use crate::types::{SharedTree, shared, Language, Range};
 
 #[defun(user_ptr)]
 fn _parser() -> Result<Parser> {
@@ -50,4 +50,31 @@ fn parse(parser: &mut Parser, read: Value, old_tree: Option<&SharedTree>) -> Res
 fn parse_string(parser: &mut Parser, s: String) -> Result<Option<SharedTree>> {
     let tree = parser.parse(s, None);
     Ok(tree.map(shared))
+}
+
+#[defun]
+fn _reset(parser: &mut Parser) -> Result<()> {
+    Ok(parser.reset())
+}
+
+#[defun]
+fn _timeout_micros(parser: &mut Parser) -> Result<u64> {
+    Ok(parser.timeout_micros())
+}
+
+#[defun]
+fn _set_timeout_micros(parser: &mut Parser, timeout_micros: u64) -> Result<()> {
+    Ok(parser.set_timeout_micros(timeout_micros))
+}
+
+#[defun]
+fn _set_included_ranges(parser: &mut Parser, ranges: Value) -> Result<()> {
+    let ranges = Vector(ranges);
+    let len = ranges.size()?;
+    let included = &mut Vec::with_capacity(len);
+    for i in 0..len {
+        let range: Range = ranges.get(i)?;
+        included.push(range.into());
+    }
+    Ok(parser.set_included_ranges(included))
 }
