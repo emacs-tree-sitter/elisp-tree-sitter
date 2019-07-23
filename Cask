5a77d37fa5d643d05ba39ed91632c61ecffce2bd
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-07-23 11:11:44 +0700

    Add Cask config and build script

---
+++ Cask
@@ -0,0 +1,8 @@
+(package-file "tree-sitter.el")
+
+(files "*.el"
+       "README.md"
+       "tree-sitter-dyn.so"
+       "Cargo.toml"
+       "Cargo.lock"
+       "src")
---
+++ bin/env.bash
@@ -0,0 +1,24 @@
+set -euo pipefail
+
+system=$(uname)
+if [[ $system == "Linux" ]]; then
+    ext="so"
+elif [[ $system == "Darwin" ]]; then
+    ext="dylib"
+else
+    echo "Unsupported system: $system"
+    exit 1
+fi
+
+here=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+
+PROJECT_ROOT=$(cd "$here/.."; pwd)
+export PROJECT_ROOT
+
+export TARGET=${TARGET:-debug}
+export MODULE_DIR="$PROJECT_ROOT/target/$TARGET"
+export MODULE_ORIGINAL=${MODULE_ORIGINAL:-libtree_sitter_dyn.$ext}
+export MODULE_NAME=${MODULE_NAME:-tree-sitter-dyn}
+export MODULE_RENAMED=${MODULE_NAME}.so
+export MODULE_FULL="$MODULE_DIR/$MODULE_RENAMED"
+export EMACS=${EMACS:-emacs}
