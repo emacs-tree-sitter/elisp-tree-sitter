bc0bc7fe6f700032e340ed5886188e7f1f1379fd
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-07-25 11:28:52 +0700

    Add more tests

--- tree-sitter-tests.el
+++ tree-sitter-tests.el
@@ -6,7 +6,7 @@
 
 ;;; Commentary:
 
-;; Tests for tree-sitter.
+;; Tests for `tree-sitter'.
 
 ;;; Code:
 
@@ -23,13 +23,19 @@
 
 (defun ts-test-full-path (relative-path)
   "Return full path from project RELATIVE-PATH."
-  (concat (file-name-as-directory (getenv "PROJECT_ROOT")) relative-path))
+  (concat (file-name-directory (locate-library "tree-sitter")) relative-path))
+
+(defmacro ts-test-with (name sym &rest body)
+  "Eval BODY with SYM bound to a new parser for language NAME."
+  (declare (indent 2))
+  `(let ((,sym (ts-test-make-parser ,name)))
+     ,@body))
 
 (ert-deftest creating-parser ()
   (should (ts-parser-p (ts-test-make-parser "rust"))))
 
-(ert-deftest parsing-rust-string ()
-  (let ((parser (ts-test-make-parser "rust")))
+(ert-deftest parsing::rust-string ()
+  (ts-test-with "rust" parser
     (let ((tree (ts-parse-string parser "fn foo() {}")))
       (should (equal (read (ts-tree-to-sexp tree))
                      '(source_file
@@ -38,37 +44,46 @@
                         (parameters)
                         (block))))))))
 
-(ert-deftest parsing-without-setting-language ()
+(ert-deftest parsing::without-setting-language ()
+  (ert-skip "Need to distinguish between this and timeout/cancellation")
   (let ((parser (ts-make-parser)))
-    (should (null (ts-parse-string parser "fn foo() {}")))))
+    (should-error (ts-parse-string parser "fn foo() {}") :type 'rust-panic)))
 
-(ert-deftest parsing-rust-buffer ()
-  (let ((parser (ts-test-make-parser "rust")))
+(ert-deftest parsing::rust-buffer ()
+  (ts-test-with "rust" parser
     (with-temp-buffer
       (insert-file-contents (ts-test-full-path "src/types.rs"))
-      (let* ((tree)
-             (initial (benchmark-run (setq tree (ts-parse parser #'ts-buffer-input nil))))
-             (reparse (benchmark-run (ts-parse parser #'ts-buffer-input tree))))
-        (message "initial %s" initial)
-        (message "reparse %s" reparse)
-        (should (> (car initial) (car reparse)))))))
+      (let* ((tree) (old-tree)
+             (initial (benchmark-run
+                          (setq tree (ts-parse parser #'ts-buffer-input nil))))
+             (reparse (benchmark-run
+                          (progn
+                            (setq old-tree tree)
+                            (setq tree (ts-parse parser #'ts-buffer-input old-tree))))))
+        ;; (message "initial %s" initial)
+        ;; (message "reparse %s" reparse)
+        (ert-info ("Same code should result in empty change ranges")
+          (should (equal [] (ts-changed-ranges tree old-tree))))
+        (ert-info ("Incremental parsing shoud be faster than initial")
+          (should (> (car initial) (car reparse))))))))
 
-(ert-deftest using-node-without-tree ()
+(ert-deftest node::using-without-tree ()
   "Test that a tree's nodes are still usable after no direct reference to the
 tree is held (since nodes internally reference the tree)."
-  (let* ((parser (ts-test-make-parser "rust")))
-    (message "timing: %s" (benchmark-run 1 (let ((node (ts-root-node
-                                      (ts-parse-string parser "fn foo() {}"))))
-                           (garbage-collect)
-                           (should (eql 1 (ts-count-children node))))))
+  (ts-test-with "rust" parser
+    (let ((node (ts-root-node
+                 (ts-parse-string parser "fn foo() {}"))))
+      (garbage-collect)
+      (should (eql 1 (ts-count-children node))))
     (garbage-collect)))
 
-(ert-deftest walk ()
-  (let* ((parser (ts-test-make-parser "rust"))
-         (tree (ts-parse-string parser "fn foo() {}"))
-         (node (ts-root-node tree)))
-    (should (ts-cursor-p (ts-make-cursor tree)))
-    (should (ts-cursor-p (ts-make-cursor node)))))
+(ert-deftest cursor::walk ()
+  (ts-test-with "rust" parser
+    (let* ((tree (ts-parse-string parser "fn foo() {}"))
+           (node (ts-root-node tree)))
+      (ert-info ("Should be able to get a cursor from either a tree or a node")
+        (should (ts-cursor-p (ts-make-cursor tree)))
+        (should (ts-cursor-p (ts-make-cursor node)))))))
 
 (provide 'tree-sitter-tests)
 ;;; tree-sitter-tests.el ends here
