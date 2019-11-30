45639d403080b7dea15392e96e103c38e8a279af
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-11-30 12:58:40 +0700

    Use vectors, instead of user-ptr, for query matches and captures

--- src/query.rs
+++ src/query.rs
@@ -22,29 +22,29 @@ fn vec_to_vector<'e, T: IntoLisp<'e>>(env: &'e Env, vec: Vec<T>) -> Result<Vecto
 ///
 /// The query is associated with LANGUAGE, and can only be run on syntax nodes
 /// parsed with LANGUAGE.
-#[defun]
-fn make_query(language: Language, source: String) -> Result<Shared<Query>> {
+#[defun(user_ptr)]
+fn make_query(language: Language, source: String) -> Result<Query> {
     // TODO: Better error message
-    Ok(shared(Query::new(language.into(), &source).unwrap()))
+    Ok(Query::new(language.into(), &source).unwrap())
 }
 
 /// Return the byte offset where the NTH pattern starts in QUERY's source.
 #[defun(mod_in_name = true)]
-fn start_byte_for_pattern(query: Borrowed<Query>, nth: usize) -> Result<usize> {
-    Ok(query.borrow().start_byte_for_pattern(nth))
+fn start_byte_for_pattern(query: &Query, nth: usize) -> Result<usize> {
+    Ok(query.start_byte_for_pattern(nth))
 }
 
 /// Return the number of patterns in QUERY.
 #[defun(mod_in_name = true)]
-fn pattern_count(query: Borrowed<Query>) -> Result<usize> {
-    Ok(query.borrow().pattern_count())
+fn pattern_count(query: &Query) -> Result<usize> {
+    Ok(query.pattern_count())
 }
 
 /// Return the names of the captures used in QUERY.
 #[defun(mod_in_name = true)]
 fn capture_names(query: Value) -> Result<Vector> {
     let env = query.env;
-    let query = query.into_rust::<Borrowed<Query>>()?.borrow();
+    let query = query.into_ref::<Query>()?;
     let names = query.capture_names();
     let len = names.len();
     let vec = make_vector(env, len)?;
@@ -59,17 +59,17 @@ fn capture_names(query: Value) -> Result<Vector> {
 /// This prevents the capture from being returned in matches, and also avoids any
 /// resource usage associated with recording the capture.
 #[defun(mod_in_name = true)]
-fn disable_captures(query: Borrowed<Query>, name: String) -> Result<()> {
-    query.borrow_mut().disable_capture(&name);
+fn disable_captures(query: &mut Query, name: String) -> Result<()> {
+    query.disable_capture(&name);
     Ok(())
 }
 
 /// Create a new cursor for executing a given query.
 ///
 /// The cursor stores the state that is needed to iteratively search for matches.
-#[defun]
-fn make_query_cursor() -> Result<Shared<QueryCursor>> {
-    Ok(shared(QueryCursor::new()))
+#[defun(user_ptr)]
+fn make_query_cursor() -> Result<QueryCursor> {
+    Ok(QueryCursor::new())
 }
 
 fn wrap_text_callback<'e>(
@@ -79,70 +79,83 @@ fn wrap_text_callback<'e>(
 ) -> impl FnMut(Node<'e>) -> String + 'e {
     move |child| {
         let child = RefCell::new(node.map(|_| child));
-        text_callback.call((child, )).and_then(|v| v.into_rust()).unwrap_or_else(|e| {
+        text_callback.call((child,)).and_then(|v| v.into_rust()).unwrap_or_else(|e| {
             error.borrow_mut().replace(e);
             "".to_owned()
         })
     }
 }
 
-
 #[defun]
 fn query_cursor_matches<'e>(
-    cursor: Borrowed<QueryCursor>,
-    query: Borrowed<Query>,
+    cursor: &mut QueryCursor,
+    query: &Query,
     node: &RNode,
     text_callback: Value<'e>,
 ) -> Result<Vector<'e>> {
-    let rquery = &*query.borrow();
     let error = RefCell::new(None);
-    let matches = cursor.borrow_mut().matches(
-        rquery,
+    let matches = cursor.matches(
+        query,
         node.borrow().clone(),
         wrap_text_callback(node, text_callback, &error),
     );
     let mut vec = vec![];
+    let env = text_callback.env;
     for m in matches {
         if let Some(error) = error.borrow_mut().take() {
             return Err(error);
         }
-        vec.push(RQueryMatch::new(node.clone_tree(), cursor.clone(), query.clone(), |_, _, _| m));
+        let _match = make_vector(env, 2)?;
+        let captures = make_vector(env, m.captures.len())?;
+        for (ci, c) in m.captures.iter().enumerate() {
+            let capture = make_vector(env, 2)?;
+            capture.set(0, c.index)?;
+            capture.set(1, RefCell::new(node.map(|_| c.node)))?;
+            captures.set(ci, capture)?;
+        }
+        _match.set(0, m.pattern_index)?;
+        _match.set(1, captures)?;
+        vec.push(_match);
     }
-    vec_to_vector(text_callback.env, vec)
+    vec_to_vector(env, vec)
 }
 
 #[defun]
 fn query_cursor_captures<'e>(
-    cursor: Borrowed<QueryCursor>,
-    query: Borrowed<Query>,
+    cursor: &mut QueryCursor,
+    query: &Query,
     node: &RNode,
     text_callback: Value<'e>,
 ) -> Result<Vector<'e>> {
-    let rquery = &*query.borrow();
     let error = RefCell::new(None);
-    let captures = cursor.borrow_mut().captures(
-        rquery,
+    let captures = cursor.captures(
+        query,
         node.borrow().clone(),
         wrap_text_callback(node, text_callback, &error),
     );
     let mut vec = vec![];
+    let env = text_callback.env;
     for (m, capture_index) in captures {
         if let Some(error) = error.borrow_mut().take() {
             return Err(error);
         }
-        vec.push(RQueryCapture::new(node.clone_tree(), |_| m.captures[capture_index]))
+        let c = m.captures[capture_index];
+        let capture = make_vector(env, 2)?;
+        capture.set(0, c.index)?;
+        capture.set(1, RefCell::new(node.map(|_| c.node)))?;
+        vec.push(capture);
     }
-    vec_to_vector(text_callback.env, vec)
+    vec_to_vector(env, vec)
 }
 
 #[defun]
-fn query_cursor_set_byte_range(cursor: Borrowed<QueryCursor>, beg: usize, end: usize) -> Result<()> {
-    cursor.borrow_mut().set_byte_range(beg, end);
+fn query_cursor_set_byte_range(cursor: &mut QueryCursor, beg: usize, end: usize) -> Result<()> {
+    cursor.set_byte_range(beg, end);
     Ok(())
 }
 
 #[defun]
-fn query_cursor_set_point_range(cursor: Borrowed<QueryCursor>, beg: Point, end: Point) -> Result<()> {
-    cursor.borrow_mut().set_point_range(beg.into(), end.into());
+fn query_cursor_set_point_range(cursor: &mut QueryCursor, beg: Point, end: Point) -> Result<()> {
+    cursor.set_point_range(beg.into(), end.into());
     Ok(())
 }
