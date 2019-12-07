016e200ec1dbb5f099af543e2157b8fd32aa6f25
Jorge Javier Araya Navarro <jorgejavieran@yahoo.com.mx>
2019-12-07 01:28:12 -0600

    Highlight matches correctly with custom face

--- tree-sitter-builder.el
+++ tree-sitter-builder.el
@@ -11,7 +11,7 @@
 
 (require 'scheme)
 (require 'tree-sitter)
-(require 'dash)
+(require 'seq)
 
 (define-derived-mode tree-sitter-builder-mode prog-mode "ts-query-builder"
   "Major mode for building tree-sitter queries and testing them live"
@@ -28,19 +28,20 @@
   "Target buffer to run the queries against")
 
 (defun tree-sitter-builder--highlight-node (node)
-  "Highlight a node match in the current buffer"
-  (set-text-properties (ts-node-start-position node) (ts-node-end-position node) '(face tree-sitter-builder-match)))
+  "Highlight a NODE match in the current buffer."
+  (let ((_node (elt node 1)))
+    (add-text-properties (ts-node-start-position _node) (ts-node-end-position _node) '(font-lock-face tree-sitter-builder-match))))
 
 (defun tree-sitter-builder--eval-query (patterns)
   "Evaluate a query PATTERNS against the target buffer."
   (with-current-buffer tree-sitter-builder--target-buffer
-    (remove-text-properties (point-min) (point-max) '(face tree-sitter-builder-match))
+    (remove-text-properties (point-min) (point-max) '(font-lock-face tree-sitter-builder-match))
     (let* ((query (ts-make-query tree-sitter-language patterns))
            (root-node (ts-root-node tree-sitter-tree))
            (matches (ts-query-captures query root-node nil nil)))
-      (if matches
+      (if (> (length matches) 0)
           (seq-doseq (match matches)
-            (tree-sitter-builder--highlight-node (elt match 1)))
+            (tree-sitter-builder--highlight-node match))
         (message "[ERR] no matches found or invalid query")))))
 
 (provide 'tree-sitter-builder)
