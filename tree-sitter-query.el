075820ef93a3fdd71e83cd7707ab754b97bacf1b
Jorge Javier Araya Navarro <jorgejavieran@yahoo.com.mx>
2020-01-03 17:16:02 -0600

    Add command that start the tree-sitter-query builder

--- tree-sitter-query.el
+++ tree-sitter-query.el
@@ -43,5 +43,27 @@
             (tree-sitter-query--highlight-node (elt match 1)))
         (message "[ERR] no matches found or invalid query")))))
 
+(defun tree-sitter-query--after-post-command ()
+  (when (eq this-command 'self-insert-command)
+    (message "this is a test")))
+
+(defun tree-sitter-query-builder ()
+  "Provide means for developers to write and test tree-sitter queries.
+
+The buffer on focus when the command is called is set as the target buffer"
+  (interactive)
+  (let ((target-buffer (current-buffer))
+        (builder-window (split-window-vertically -7))
+        (builder-buffer (get-buffer-create "*tree-sitter-query-builder*")))
+    (with-selected-window builder-window
+      (switch-to-buffer builder-buffer))
+    (with-current-buffer builder-buffer
+      (erase-buffer)
+      (tree-sitter-query-mode)
+      (insert "[]")
+      (add-hook post-command-hook 'tree-sitter-query--after-post-command nil t))
+    (setf tree-sitter-query--target-buffer target-buffer)
+    (select-window builder-window)))
+
 (provide 'tree-sitter-query)
 ;;; tree-sitter-query.el ends here
