ee9d16fddddfce78de59bb7f720a04a3586d0bfa
Jorge Javier Araya Navarro <jorgejavieran@yahoo.com.mx>
2020-01-03 14:05:18 -0600

    Rename file, functions and variables

--- tree-sitter-query.el
+++ tree-sitter-query.el
@@ -13,36 +13,35 @@
 (require 'tree-sitter)
 (require 'seq)
 
-(define-derived-mode tree-sitter-builder-mode prog-mode "ts-query-builder"
+(define-derived-mode tree-sitter-query-mode prog-mode "ts-query-builder"
   "Major mode for building tree-sitter queries and testing them live"
   :syntax-table scheme-mode-syntax-table
   :abbrev-table scheme-mode-abbrev-table)
 
-(defface tree-sitter-builder-match
+(defface tree-sitter-query-match
   '((t
      (:underline
       (:color "red" :style line))))
   "face for highlighting matches")
 
-(defvar tree-sitter-builder--target-buffer nil
+(defvar tree-sitter-query--target-buffer nil
   "Target buffer to run the queries against")
 
-(defun tree-sitter-builder--highlight-node (node)
-  "Highlight a NODE match in the current buffer."
-  (let ((_node (elt node 1)))
-    (add-text-properties (ts-node-start-position _node) (ts-node-end-position _node) '(font-lock-face tree-sitter-builder-match))))
+(defun tree-sitter-query--highlight-node (node)
+  "Highlight a node match in the current buffer"
+  (set-text-properties (ts-node-start-position node) (ts-node-end-position node) '(face tree-sitter-query-match)))
 
-(defun tree-sitter-builder--eval-query (patterns)
+(defun tree-sitter-query--eval-query (patterns)
   "Evaluate a query PATTERNS against the target buffer."
-  (with-current-buffer tree-sitter-builder--target-buffer
-    (remove-text-properties (point-min) (point-max) '(font-lock-face tree-sitter-builder-match))
+  (with-current-buffer tree-sitter-query--target-buffer
+    (remove-text-properties (point-min) (point-max) '(face tree-sitter-query-match))
     (let* ((query (ts-make-query tree-sitter-language patterns))
            (root-node (ts-root-node tree-sitter-tree))
            (matches (ts-query-captures query root-node nil nil)))
       (if (> (length matches) 0)
           (seq-doseq (match matches)
-            (tree-sitter-builder--highlight-node match))
+            (tree-sitter-query--highlight-node (elt match 1)))
         (message "[ERR] no matches found or invalid query")))))
 
-(provide 'tree-sitter-builder)
-;;; tree-sitter-builder.el ends here
+(provide 'tree-sitter-query)
+;;; tree-sitter-query.el ends here
