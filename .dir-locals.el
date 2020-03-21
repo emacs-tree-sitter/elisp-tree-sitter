56861af5a45437c7f329b9aee0df5136461bd9db
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2020-03-21 14:52:19 +0700

    Specify code style via .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode
+  (indent-tabs-mode . nil)
+  (sentence-end-double-space . nil)
+  (fill-column . 80)
+  (emacs-lisp-docstring-fill-column . 80)
+  (comment-fill-column . 80)))
