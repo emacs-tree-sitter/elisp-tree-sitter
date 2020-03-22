ee48e84d57aef1f11c82abb5aa6439b4a735e8eb
Jorge Javier Araya Navarro <jorge@esavara.cr>
2020-03-22 16:05:57 -0600

    Address most feedback from @ubolonton
    the only thing left out was a comment on using a different hook for when changes happen.
    
    - https://github.com/ubolonton/emacs-tree-sitter/pull/15#discussion_r395970775

--- lisp/tree-sitter-query.el
+++ lisp/tree-sitter-query.el
@@ -1,8 +1,14 @@
-;; tree-sitter-query.el --- tools for running queries live -*- lexical-binding: t; coding: utf-8 -*-
+;;; tree-sitter-query.el --- Tools for running queries live -*- lexical-binding: t; coding: utf-8 -*-
 
 ;; Copyright (C) 2020  Tuấn-Anh Nguyễn
 ;;
 ;; Author: Jorge Javier Araya Navarro <jorgejavieran@yahoo.com.mx>
+;; Homepage: https://github.com/ubolonton/emacs-tree-sitter
+;; Version: 0.4.0
+;; Package-Requires: ((emacs "25.1"))
+;; License: MIT
+
+;;; Commentary:
 
 ;; This file contains other debug utilities for building queries and see
 ;; results in a target buffer
@@ -12,7 +18,10 @@
 (require 'scheme)
 (require 'cl-lib)
 (require 'tree-sitter)
-(require 'tree-sitter-query-faces)
+
+(defgroup tree-sitter-query nil
+  "tree-sitter playground for GNU Emacs."
+  :group 'tree-sitter)
 
 (define-derived-mode tree-sitter-query-mode prog-mode "ts-query-builder"
   "Major mode for building tree-sitter queries and testing them live"
@@ -20,31 +29,27 @@
   :abbrev-table scheme-mode-abbrev-table)
 
 (defconst tree-sitter-query-builder-buffer-name "*tree-sitter-query-builder*"
-  "Name of the builder buffer")
+  "Name of the builder buffer.")
 
 (defvar tree-sitter-query--target-buffer nil
   "Target buffer to run the queries against.")
 
-(defvar tree-sitter-query--match-highlight-number -1
-  "Counter that keeps track of the number of the match color face.")
+(defface tree-sitter-query-match
+  '((t :foreground "#000"
+       :background "#00bfff"
+       :weight bold))
+  "face for match highlight"
+  :group 'tree-sitter-query)
 
-(defun tree-sitter-query--get-next-match-highlight-color ()
-  "Return the symbol for the next highlight match face in number order."
-  (setq tree-sitter-query--match-highlight-number
-        (mod (cl-incf tree-sitter-query--match-highlight-number) tree-sitter-query-faces-list-length))
-  (elt tree-sitter-query-faces-match-list tree-sitter-query--match-highlight-number))
-
-(defun tree-sitter-query--highlight-node (node match-face)
-  "Highlight a NODE match in the current buffer with face MATCH-FACE."
-  (let* ((capture-name (car node))
-         (captured-node (cdr node))
-         (node-start (ts-node-start-position captured-node))
-         (node-end (ts-node-end-position captured-node))
-         (overlay-added (make-overlay node-start node-end)))
+(defun tree-sitter-query--highlight-capture (capture)
+  "Highlight a CAPTURE match in the current buffer."
+  (pcase-let* ((`(,capture-name . ,captured-node) capture)
+               (`(,node-start . ,node-end) (ts-node-position-range captured-node))
+               (overlay-added (make-overlay node-start node-end)))
     ;; ensure it is deleted automatically when the overlay becomes empty
     (overlay-put overlay-added 'evaporate t)
     ;; set the match-face as the face of the overlay
-    (overlay-put overlay-added 'face match-face)
+    (overlay-put overlay-added 'face 'tree-sitter-query-match)
     ;; put the name of the capture in the help-echo, if any
     (unless (string= capture-name "")
       (overlay-put overlay-added 'help-echo capture-name))))
@@ -56,32 +61,31 @@
     (remove-overlays)
     (let* ((query (ts-make-query tree-sitter-language patterns))
            (root-node (ts-root-node tree-sitter-tree))
-           (matches (ts-query-captures query root-node nil nil))
-           (nextface))
-      (if (= (length matches) 0)
+           (captures-list (ts-query-captures query root-node)))
+      (if (= (length captures-list) 0)
           (message "no matches found")
-        ;; reset counter
-        (setq tree-sitter-query--match-highlight-number -1)
         ;; iterate all matches and highlight them with an underline
         (cl-loop
-         for submatches across matches
-         do (setq nextface (tree-sitter-query--get-next-match-highlight-color))
+         for captures across captures-list
+         do
          (cl-loop
-          for match on submatches
-          do (tree-sitter-query--highlight-node match nextface)))))))
+          for capture on captures
+          do (tree-sitter-query--highlight-capture capture)))))))
 
 (defun tree-sitter-query--after-change (&rest args)
   "Run evaluation of pattern in current buffer for every change made by the user, ignoring ARGS."
   (with-current-buffer (get-buffer tree-sitter-query-builder-buffer-name)
-    (let ((pattern (buffer-substring-no-properties (point-min) (point-max))))
-      (with-demoted-errors
-          (tree-sitter-query--eval-query pattern)))))
+    (let ((pattern (buffer-string)))
+      (with-demoted-errors "Error: %S"
+        (tree-sitter-query--eval-query pattern)))))
 
 (defun tree-sitter-query--clean-target-buffer ()
-  "Remove all overlays if the builder buffer happens to be killed"
+  "Remove all overlays if the builder buffer happens to be killed."
   (with-current-buffer tree-sitter-query--target-buffer
-    (remove-overlays)))
+    (remove-overlays))
+  (setq tree-sitter-query--target-buffer nil))
 
+;;;###autoload
 (defun tree-sitter-query-builder ()
   "Provide means for developers to write and test tree-sitter queries.
 
@@ -92,17 +96,17 @@ The buffer on focus when the command is called is set as the target buffer"
          (builder-window-is-visible (get-buffer-window builder-buffer)))
     (when (eq target-buffer builder-buffer)
       (user-error "This buffer cannot be use as target buffer"))
+    (with-current-buffer target-buffer
+      (unless tree-sitter-mode
+        (tree-sitter-mode))
+      (add-hook 'after-change-functions 'tree-sitter-query--after-change nil :local)
+      (setq tree-sitter-query--target-buffer target-buffer))
     (unless builder-window-is-visible
       (unless (display-buffer-in-side-window
                builder-buffer
                '((side . bottom)
                  (window-height . 10)))
         (user-error "Not enough space available for query builder window")))
-    (with-current-buffer target-buffer
-      (unless tree-sitter-mode
-        (tree-sitter-mode))
-      (add-hook 'after-change-functions 'tree-sitter-query--after-change nil :local)
-      (setq tree-sitter-query--target-buffer target-buffer))
     (with-current-buffer builder-buffer
       (erase-buffer)
       (tree-sitter-query-mode)
