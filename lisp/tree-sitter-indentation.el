10365e0df68c4e4a5a1c5d3b87d0e1bcbc961e5e
Jorge Javier Araya Navarro <jorge@esavara.cr>
2020-03-30 18:16:27 -0600

    Define a subgroup

---
+++ lisp/tree-sitter-indentation.el
@@ -0,0 +1,21 @@
+;;; tree-sitter-indentation.el --- Better and smart indentation for major modes
+
+;; Version: 0.5.0
+;; Homepage: https://github.com/ubolonton/emacs-tree-sitter
+;; Package-Requires: ((emacs "25.1"))
+
+;;; Commentary:
+
+;; better and smart indentation for major modes
+
+;;; Code:
+
+(require 'tree-sitter)
+
+(defgroup tree-sitter-indentation nil
+  "indentation for major modes with tree-sitter."
+  :group 'tree-sitter)
+
+
+(provide 'tree-sitter-indentation)
+;;; tree-sitter-indentation.el ends here
