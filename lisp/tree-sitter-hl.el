4ff76fe8e7769e9dfd23da38477c93f1c53cad70
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2020-04-08 09:40:05 +0700

    Make core highlighting logic more or less working

--- lisp/tree-sitter-hl.el
+++ lisp/tree-sitter-hl.el
@@ -119,29 +119,29 @@
 
 (defcustom tree-sitter-hl-default-faces
   '(("attribute"             . tree-sitter-attribute-face)
-     ("comment"               . tree-sitter-comment-face)
-     ("constant"              . tree-sitter-constant-face)
-     ("constant.builtin"      . tree-sitter-constant-builtin-face)
-     ("constructor"           . tree-sitter-constructor-face)
-     ("escape"                . tree-sitter-escape-face)
-     ("function"              . tree-sitter-function-face)
-     ("function.builtin"      . tree-sitter-function-builtin-face)
-     ("function.macro"        . tree-sitter-function-macro-face)
-     ("function.method"       . tree-sitter-function-method-face)
-     ("identifier"            . tree-sitter-identifier-face)
-     ("keyword"               . tree-sitter-keyword-face)
-     ("label"                 . tree-sitter-label-face)
-     ("operator"              . tree-sitter-operator-face)
-     ("property"              . tree-sitter-property-face)
-     ("punctuation"           . tree-sitter-punctuation-face)
-     ("punctuation.bracket"   . tree-sitter-punctuation-bracket-face)
-     ("punctuation.delimiter" . tree-sitter-punctuation-delimiter-face)
-     ("string"                . tree-sitter-string-face)
-     ("type"                  . tree-sitter-type-face)
-     ("type.builtin"          . tree-sitter-type-builtin-face)
-     ("variable"              . tree-sitter-variable-face)
-     ("variable.builtin"      . tree-sitter-variable-builtin-face)
-     ("variable.parameter"    . tree-sitter-variable-parameter-face))
+    ("comment"               . tree-sitter-comment-face)
+    ("constant"              . tree-sitter-constant-face)
+    ("constant.builtin"      . tree-sitter-constant-builtin-face)
+    ("constructor"           . tree-sitter-constructor-face)
+    ("escape"                . tree-sitter-escape-face)
+    ("function"              . tree-sitter-function-face)
+    ("function.builtin"      . tree-sitter-function-builtin-face)
+    ("function.macro"        . tree-sitter-function-macro-face)
+    ("function.method"       . tree-sitter-function-method-face)
+    ("identifier"            . tree-sitter-identifier-face)
+    ("keyword"               . tree-sitter-keyword-face)
+    ("label"                 . tree-sitter-label-face)
+    ("operator"              . tree-sitter-operator-face)
+    ("property"              . tree-sitter-property-face)
+    ("punctuation"           . tree-sitter-punctuation-face)
+    ("punctuation.bracket"   . tree-sitter-punctuation-bracket-face)
+    ("punctuation.delimiter" . tree-sitter-punctuation-delimiter-face)
+    ("string"                . tree-sitter-string-face)
+    ("type"                  . tree-sitter-type-face)
+    ("type.builtin"          . tree-sitter-type-builtin-face)
+    ("variable"              . tree-sitter-variable-face)
+    ("variable.builtin"      . tree-sitter-variable-builtin-face)
+    ("variable.parameter"    . tree-sitter-variable-parameter-face))
   "Alist of query identifier to face used for highlighting matches."
   :type '(alist :key-type string
                 :value-type face)
@@ -159,7 +159,11 @@
                (`(,beg . ,end) (ts-node-position-range node)))
     ;; (message " %s <- %s <- [%s %s]" face name beg end)
     (when face
-      (add-face-text-property beg end face))))
+      (put-text-property beg end 'face face)
+      ;; Naive `add-face-text-property' keeps adding the same face, slowing
+      ;; things down.
+      ;; (add-face-text-property beg end face)
+      )))
 
 (defun tree-sitter-hl--highlight-region (beg end &optional _loudly)
   (message "tree-sitter-hl [%s %s]" beg end)
@@ -174,28 +178,103 @@
                       tree-sitter-hl--query-cursor
                       nil
                       #'ts--node-text)))
+      ;; TODO: Handle quitting.
       (with-silent-modifications
+        (font-lock-unfontify-region beg end)
+        ;; TODO: Handle uncaptured nodes.
         (seq-do #'tree-sitter-hl--highlight-capture captures))
+      ;; TODO: Return the actual region being fontified.
       nil)))
 
+(defvar tree-sitter-hl--changed-ranges)
+
+
+(defun tree-sitter-hl--extend-after-change-region (beg end _old-len)
+  "Hook meant for `jit-lock-after-change-extend-region-functions'.
+This relies on `tree-sitter-hl--record-changed-ranges' to have been run first."
+  (when (bound-and-true-p tree-sitter-hl--changed-ranges)
+    (message "tree-sitter-hl extend %s %s" beg end)
+    (seq-doseq (range tree-sitter-hl--changed-ranges)
+      (pcase-let ((`[,beg-byte ,end-byte] range))
+        (setq beg (min beg (byte-to-position beg-byte)))
+        (setq end (min end (byte-to-position end-byte)))))
+    (message "                      %s %s" beg end)
+    ;; TODO: Just set it to nil?
+    (makunbound 'tree-sitter-hl--changed-ranges)
+    `(,beg . ,end)))
+
+(defun tree-sitter-hl--record-changed-ranges (old-tree)
+  (message "tree-sitter-hl record")
+  ;; TODO: What happens if more changes are made before these changes are
+  ;; handled?
+  (setq tree-sitter-hl--changed-ranges
+        (ts-changed-ranges old-tree tree-sitter-tree)))
+
+(defun tree-sitter-hl--invalidate-ranges (old-tree)
+  (when old-tree
+    (seq-doseq (range (ts-changed-ranges old-tree tree-sitter-tree))
+      ;; TODO: How about invalidating a single large range?
+      (pcase-let* ((`[,beg-byte ,end-byte] range)
+                   (beg (byte-to-position beg-byte))
+                   (end (byte-to-position end-byte)))
+        (message "tree-sitter-hl invalidate [%s %s]" beg end)
+        ;; TODO: How about calling `jit-lock-refontify' directly?
+        (font-lock-flush beg end)))))
+
+;;; This assumes both `tree-sitter-mode' and `font-lock-mode' were already
+;;; enabled in the buffer.
 (defun tree-sitter-hl--enable ()
-  ;; XXX
-  (setq tree-sitter-hl--query
-        (pcase major-mode
-          ('rust-mode tree-sitter-hl--rust-query)
-          ('c-mode tree-sitter-hl--c-query)))
+  (unless tree-sitter-hl--query
+    ;; XXX
+    (let ((lang-symbol (alist-get major-mode tree-sitter-major-mode-language-alist)))
+      (setq tree-sitter-hl--query (tree-sitter-hl--query-for lang-symbol))))
 
   (setq tree-sitter-hl--query-cursor (ts-make-query-cursor))
 
+  ;; TODO: Override `font-lock-extend-after-change-region-function', or hook
+  ;; into `jit-lock-after-change-extend-region-functions' directly. For that to
+  ;; work, we need to make sure `tree-sitter--after-change' runs before
+  ;; `jit-lock-after-change'.
+  (add-hook 'tree-sitter-after-change-functions
+            #'tree-sitter-hl--invalidate-ranges
+            nil :local)
+
+  ;; (add-hook 'tree-sitter-after-change-functions
+  ;;           #'tree-sitter-hl--record-changed-ranges
+  ;;           nil :local)
+  ;; (add-function :override (local 'font-lock-extend-after-change-region-function)
+  ;;               #'tree-sitter-hl--extend-after-change-region)
+
   ;; XXX
   (add-function :override (local 'font-lock-fontify-region-function)
-                #'tree-sitter-hl--highlight-region))
+                #'tree-sitter-hl--highlight-region)
+
+  (font-lock-flush))
 
 (defun tree-sitter-hl--disable ()
-  (setq tree-sitter-hl--query-cursor nil)
-
   (remove-function (local 'font-lock-fontify-region-function)
-                   #'tree-sitter-hl--highlight-region))
+                   #'tree-sitter-hl--highlight-region)
+
+  ;; (remove-function (local 'font-lock-extend-after-change-region-function)
+  ;;                  #'tree-sitter-hl--extend-after-change-region)
+  ;; (remove-hook 'tree-sitter-after-change-functions
+  ;;              #'tree-sitter-hl--record-changed-ranges)
+
+  (setq tree-sitter-hl--query-cursor nil)
+  (setq tree-sitter-hl--query nil)
+
+  (font-lock-flush))
+
+;;; XXX
+(defun tree-sitter-hl--query-for (lang-symbol)
+  (let* ((query-path (concat
+                      (file-name-directory (locate-library "tree-sitter-langs"))
+                      (format "/repos/tree-sitter-%s/queries/highlights.scm"
+                              lang-symbol)))
+         (query-string (with-temp-buffer
+                         (insert-file-contents query-path)
+                         (buffer-string))))
+    (ts-make-query (tree-sitter-require lang-symbol) query-string)))
 
 (provide 'tree-sitter-hl)
 ;;; tree-sitter-hl.el ends here
