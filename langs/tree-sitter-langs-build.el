98d6b41a5b72eefa4b305e8be4876285021c6f92
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2020-10-23 16:00:32 +0700

    Stash changes in grammar repos when building grammar binaries

--- langs/tree-sitter-langs-build.el
+++ langs/tree-sitter-langs-build.el
@@ -212,7 +212,8 @@ This function requires git and tree-sitter CLI."
           (tree-sitter-langs--call "git" "remote" "-v" "update"))
       (tree-sitter-langs--call "git" "clone" "-q" repo dir))
     (let ((default-directory dir))
-      (tree-sitter-langs--call "git" "reset" "--hard" version)
+      (tree-sitter-langs--call "git" "stash" "push")
+      (tree-sitter-langs--call "git" "checkout" version)
       (tree-sitter-langs--call "npm" "set" "progress=false")
       ;; TODO: Figure out why we need to skip `npm install' for some repos.
       (ignore-errors
