5671a0e441cfcb8ba731f08d635564ceb6975492
Jen-Chieh <jcs090218@gmail.com>
2021-01-04 13:39:17 +0800

    Propos use shell-command instead of tar-mode.

--- langs/tree-sitter-langs-build.el
+++ langs/tree-sitter-langs-build.el
@@ -339,13 +339,9 @@ non-nil."
       ;; FIX: Handle HTTP errors properly.
       (url-copy-file (tree-sitter-langs--bundle-url version os)
                      bundle-file 'ok-if-already-exists)
-      (unless
-          (ignore-errors
-            (with-temp-buffer
-              (insert-file-contents bundle-file) (tar-mode) (tar-untar-buffer)))
-        (shell-command (format "tar -xvzf %s"
-                               (shell-quote-argument
-                                (concat default-directory bundle-file)))))
+      (shell-command (format "tar -xvzf %s"
+                             (shell-quote-argument
+                              (concat default-directory bundle-file))))
       ;; FIX: This should be a metadata file in the bundle itself.
       (with-temp-file tree-sitter-langs--bundle-version-file
         (let ((coding-system-for-write 'utf-8))
