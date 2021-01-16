d569763c143fdf4ba8480befbb4b8ce1e49df5e2
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-01-16 13:21:48 +0700

    Release 0.13.1

--- lisp/tree-sitter.el
+++ lisp/tree-sitter.el
@@ -5,8 +5,8 @@
 ;; Author: Tuấn-Anh Nguyễn <ubolonton@gmail.com>
 ;; Keywords: languages tools parsers tree-sitter
 ;; Homepage: https://github.com/ubolonton/emacs-tree-sitter
-;; Version: 0.13.0
-;; Package-Requires: ((emacs "25.1") (tsc "0.13.0"))
+;; Version: 0.13.1
+;; Package-Requires: ((emacs "25.1") (tsc "0.13.1"))
 ;; SPDX-License-Identifier: MIT
 
 ;;; Commentary:
