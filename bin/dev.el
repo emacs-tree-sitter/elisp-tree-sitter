14e2411d7541eed63864d1761ce8cb4d099ee3e5
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-09-19 10:49:02 +0700

    Add convenient script/dev for tasks that don't need to be portable

--- bin/dev.el
+++
@@ -1,9 +0,0 @@
-(defun :release-notes (version &optional changelog-path)
-  (with-temp-buffer
-    (insert-file-contents (or changelog-path "CHANGELOG.md"))
-    (re-search-forward (format "## .*%s.*" version))
-    (forward-line)
-    (beginning-of-line)
-    (let ((start (point)))
-      (re-search-forward "^##")
-      (buffer-substring-no-properties start (1- (match-beginning 0))))))
