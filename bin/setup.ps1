8dd536e664b3260c4945b72c447fd8b90cd7bd66
JenChieh <jcs090218@gmail.com>
2023-09-08 16:10:55 -0700

    no need to create dir

--- bin/setup.ps1
+++ bin/setup.ps1
@@ -4,8 +4,8 @@ $project_root = (Get-Item $here).Parent.FullName
 Push-Location $project_root
 try {
     # XXX: Create the directory because `eask link` doesn't.
-    $eask_package_dir = ((eask package-directory) | Out-String).Trim()
-    New-Item -ItemType Directory -Force -Path "$eask_package_dir"
+    #$eask_package_dir = ((eask package-directory) | Out-String).Trim()
+    #New-Item -ItemType Directory -Force -Path "$eask_package_dir"
     eask link tsc core
     eask install
 } finally {
