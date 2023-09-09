89b768cbeed09e959ca3561972cd26b496369ac6
JenChieh <jcs090218@gmail.com>
2023-09-09 01:44:44 -0700

    chore: Remove Cask related files

--- .github/script/setup-cask.ps1
+++
@@ -1,2 +0,0 @@
-git clone -b github-actions https://github.com/ubolonton/cask "$env:UserProfile\.cask"
-echo "$env:UserProfile\.cask\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
