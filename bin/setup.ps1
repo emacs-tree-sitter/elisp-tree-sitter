cc12de34803ee8c7e5376c41ec171dc484762c27
Jen-Chieh Shen <jcs090218@gmail.com>
2024-12-10 17:33:08 +0800

    fix: macos-12 deprecation (#289)
    * fix: macos-12 deprecation
    
    * fix: Emacs 27.x is no longer provides proper binaries
    
    * fix: eask link command

--- bin/setup.ps1
+++ bin/setup.ps1
@@ -6,7 +6,7 @@ try {
     # XXX: Create the directory because `eask link` doesn't.
     #$eask_package_dir = ((eask package-directory) | Out-String).Trim()
     #New-Item -ItemType Directory -Force -Path "$eask_package_dir"
-    eask link tsc core
+    eask link add tsc core
     eask install
 } finally {
     Pop-Location
