d991ab85c247e2decf180d6485461f19b4d0c6b1
JenChieh <jcs090218@gmail.com>
2026-01-16 07:03:51 +0800

    Release 0.19.4

--- lisp/tree-sitter.el
+++ lisp/tree-sitter.el
@@ -6,8 +6,8 @@
 ;; Maintainer: Jen-Chieh Shen <jcs090218@gmail.com>
 ;; Keywords: languages tools parsers tree-sitter
 ;; Homepage: https://github.com/emacs-tree-sitter/elisp-tree-sitter
-;; Version: 0.19.3
-;; Package-Requires: ((emacs "27.1") (tsc "0.19.3"))
+;; Version: 0.19.4
+;; Package-Requires: ((emacs "27.1") (tsc "0.19.4"))
 ;; SPDX-License-Identifier: MIT
 
 ;;; Commentary:
